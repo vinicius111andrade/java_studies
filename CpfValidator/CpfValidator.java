7123316bbe6ecea18ba54291509693d3b7e426d9
Vinicius Andrade <vinicius111andrade@gmail.com>
2023-08-21 19:17:53 -0300

    finish code

--- CpfValidator/CpfValidator.java
+++ CpfValidator/CpfValidator.java
@@ -1,63 +1,102 @@
-package CpfValidator;
+import java.util.Scanner;
 
 public class CpfValidator {
 
-    static final int minimumCpfSize = 10;
-    static final int maximumCpfSize = 11;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um CPF (apenas números): ");
+        String cpf = scanner.nextLine();
 
-    public static boolean isCpfValid(int[] cpfArray) {
+        if (isCpfValid(cpf)) {
+            System.out.println("CPF válido.");
+        } else {
+            System.out.println("CPF inválido.");
+        }
 
-        if(!isCpfSizeCorrect(cpfArray)) {
+       scanner.close();
+    }
+
+    public static boolean isCpfValid(int[] cpf) {
+
+        if(!cpfHasCorrectLength(cpf) || cpfHasRepeatedNumberOnly(cpf)) {
             return false;
-        } else if (!isFirstVerifierDigitCorrect(cpfArray)) {
-            return false;
+        }
+
+        boolean isCpfValid = cpfHasCorrectVerifierDigits(
+            calculateCpfFirstVerifierDigit(cpf),
+            calculateCpfSecondVerifierDigit(cpf),
+            cpf
+        );
+
+        return isCpfValid;
+    }
+
+    public static boolean isCpfValid(String cpf) {
+        int[] digits = buildIntArrayFromString(cpf);
+
+        return isCpfValid(digits);
+    }
+
+    private static int[] buildIntArrayFromString(String string) {
+        int length = string.length();
+        int[] intArray = new int[length];
+        int index = 0;
+
+        while(index < length) {
+            intArray[index] = Character.getNumericValue(string.charAt(index));
+            index++;
+        }
+
+        return intArray;
+    }
+
+    private static boolean cpfHasCorrectLength(int[] cpf) {
+        return cpf.length == 11;
+    }
+
+    private static boolean cpfHasRepeatedNumberOnly(int[] cpf) {
+        int first = cpf[0];
+        int index = 0;
+
+        while(index < cpf.length) {
+            if(first != cpf[index]) {
+                return false;
+            }
+            index++;
         }
 
         return true;
-  }
-
-    private static boolean isCpfSizeCorrect(int[] cpfArray) {
-        int cpfSize = cpfArray.length;
-        return cpfSize >= minimumCpfSize && cpfSize <= maximumCpfSize;
     }
 
-    private static boolean isFirstVerifierDigitCorrect(int[] cpfArray) {
-
-        int firstVerifierDigit = calculateFirstVerifierDigit(cpfArray);
-
-        boolean isFirstDigitCorrect = firstVerifierDigit == cpfArray[9];
-
-        return isFirstDigitCorrect;
-    }
-
-    private static int calculateFirstVerifierDigit(int[] cpfArray) {
-
-        int sum = calculateCpfSpecialSum(cpfArray);
-        int remainder = sum % 11;
-        int firstVerifierDigit;
-
-        if(remainder < 2) {
-            firstVerifierDigit = 0;
-        } else {
-            firstVerifierDigit = 11 - remainder;
+    private static int calculateCpfFirstVerifierDigit(int[] cpf) {
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += cpf[i] * (10 - i);
         }
+
+        int remainder = sum % 11;
+        int firstVerifierDigit = (remainder < 2) ? 0 : 11 - remainder;
 
         return firstVerifierDigit;
     }
 
-
-    private static int calculateCpfSpecialSum(int[] cpfArray) {
-
-        int[] weights = {10,9,8,7,6,5,4,3,2};
-
-        int index = 0;
+    private static int calculateCpfSecondVerifierDigit(int[] cpf) {
         int sum = 0;
-
-        while(index < (weights.length - 1)) {
-            sum += cpfArray[index] * weights[index];
+        for (int i = 0; i < 10; i++) {
+            sum += cpf[i] * (11 - i);
         }
 
-        return sum;
+        int remainder = sum % 11;
+        int secondVerifierDigit = (remainder < 2) ? 0 : 11 - remainder;
+
+        return secondVerifierDigit;
+    }
+
+    private static boolean cpfHasCorrectVerifierDigits(
+        int firstDigit,
+        int secondDigit,
+        int[] cpf) {
+        return cpf[9] == firstDigit && cpf[10] == secondDigit;
     }
 
 }
